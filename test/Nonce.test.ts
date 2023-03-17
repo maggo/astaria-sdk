7c84ac4d6ee138b9f4cd17bf3bfe67a5f9042bfd
Justin Greenberg <me@justingreenberg.com>
2023-03-16 22:45:29 -0400

    feat: increase timeouts

--- test/Nonce.test.ts
+++ test/Nonce.test.ts
@@ -2,13 +2,15 @@ import { ethers, Wallet } from 'ethers'
 import { VaultImplementation__factory } from '../src/contracts/factories/VaultImplementation__factory'
 // test file name is changed to prevent running in the CI
 describe('Nonce', () => {
-  test('signs merkle tree root using local', async () => {
+  jest.setTimeout(90 * 1000)
+
+  test('signs merkle tree root using remote', async () => {
     //don't worry there isn't any real money here
     const privateKey =
       '95067b810f517769c5074b9b7977252154ec273cef5e79f65a7544b69a136836'
 
     const network = 'goerli'
-    const apiKey = 'QDsg62FF8hsq9JjzCRs1Oi-1ZBPKmx2U'
+    const apiKey = 'QDsg62FF8hsq9JjzCRs1Oi-1ZBPKmx2U' // TODO: REMOVE THIS
     const provider = new ethers.providers.AlchemyProvider(network, apiKey)
     const wallet = new Wallet(privateKey, provider)
 
@@ -16,6 +18,7 @@ describe('Nonce', () => {
       '0xc3F235148C06e930D99Fc5eaB08F8ADB31e0B335',
       wallet
     )
+
     const txn = await contract.getStrategistNonce()
     console.log(txn)
   })
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -27,7 +27,7 @@ const localRoot =
   '0x276a20acb0e3b40e3e98b20030585add5dd1c6c6f53b99b3bc0645809dd3eef0'
 
 describe('util.signRoot using remote', () => {
-  jest.setTimeout(30 * 1000)
+  jest.setTimeout(90 * 1000)
 
   test('signs merkle tree root', async () => {
     const provider = new MockProvider({
